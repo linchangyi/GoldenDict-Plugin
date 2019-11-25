2baca02d3847ffcd9697d800f3578be485ca3b87
lcy <talentlcy@outlook.com>
2019-11-26 07:01:18 +0800

    Revert "修改链接"
    This reverts commit c306d16370f0f8928b7a4545b36f5ab7aa4f13ce.

--- youdao/yd_get.py
+++ youdao/yd_get.py
@@ -6,19 +6,19 @@ import requests
 from lxml import etree
 from sys import argv
 
+URL = "http://dict.youdao.com/w/eng/{}"
+
 
 def translate(words):
     """函数说明：
     因为采用 get 方式 url 中要过滤掉 / 换成全角。否则引起url的解析错误。
     response.text 是 bytes 数据类型
     """
-    url = "http://dict.youdao.com/"
-
     # headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/45.0.2454.93 Safari/537.36'}
     headers = {
         'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Ubuntu Chromium/70.0.3538.67 Chrome/70.0.3538.67 Safari/537.36'}
     words = words.replace("/", "／")
-    url = url + 'w/' + words
+    url = URL.format(words)
 
     response = requests.get(url, headers)
     selector = etree.HTML(response.text)  # 生成 selector  对象, 利用 xpath 获得内容
@@ -38,14 +38,10 @@ def translate(words):
         ('id', 'ads'),
     ]
     for hidden in hiddens:
-        eles = selector.xpath("//div[@%s='%s']" % hidden)
-        if eles:
-            eles[0].attrib['style'] = 'display:none'
+        selector.xpath("//div[@%s='%s']" % hidden)[0].attrib['style'] = 'display:none'
 
     content = etree.tostring(selector, encoding='utf-8', method='html').decode('utf8')
     content = re.sub(r'href="(?:http://dict\.youdao\.com)?/w(?:/eng)?/(.*?)/', r'href="\1"', content)
-    content = content.replace('href="/example', 'href="http://dict.youdao.com/example')
-
     return content
 
 
@@ -53,8 +49,5 @@ if __name__ == "__main__":
     """
     argv[1] 获得控制器输入的第一个参数
     """
-    if len(argv) > 1:
-        result = translate(argv[1])
-    else:
-        result = translate('example_apple')
+    result = translate(argv[1])
     print(result)
